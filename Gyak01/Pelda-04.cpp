48f1715158e3d1845f47f8f462091fa3e78ebcc2
Adam Csapo <csapo.adam@gmail.com>
2018-09-06 15:02:29 +0200

    add Ember struct with vector printing function

--- Gyak01/Pelda-04.cpp
+++ Gyak01/Pelda-04.cpp
@@ -5,10 +5,30 @@
 #include<iostream>
 #include<vector>
 #include<algorithm>
+#include<string>
 
 void printVector(std::vector<int> v) {
 	for (std::vector<int>::const_iterator i = v.begin(); i != v.end(); i++) {
 		std::cout << *i << " ";
+	}
+	std::cout << std::endl;
+}
+
+struct Ember {
+	std::string nev;
+	int eletkor;
+	Ember(std::string n, int e) {
+		nev = n;
+		eletkor = e;
+	}
+	void oregit(int evek) {
+		eletkor = eletkor + evek;
+	}
+};
+
+void printEmberVector(std::vector<Ember> v) {
+	for (std::vector<Ember>::const_iterator i = v.begin(); i != v.end(); i++) {
+		std::cout << "<" << (*i).nev << ": " << (*i).eletkor << ">, ";
 	}
 	std::cout << std::endl;
 }
@@ -28,6 +48,16 @@ int main()
 	printVector(vec);
 	std::sort(vec.begin(), vec.end());
 	printVector(vec);
+
+	std::vector<Ember> embervec;
+	Ember e1("Pali", 55);
+	Ember e2("Zoli", 22);
+	Ember e3("Anna", 33);
+	embervec.push_back(e1);
+	embervec.push_back(e2);
+	embervec.push_back(e3);
+
+	printEmberVector(embervec);
 	
 	char c;
 	std::cin >> c;
