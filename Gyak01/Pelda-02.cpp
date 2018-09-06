8b38e87be35263b4c713c499cfc9acb5235eafd4
Adam Csapo <csapo.adam@gmail.com>
2018-09-06 13:45:14 +0200

    comments to pointer example

--- Gyak01/Pelda-02.cpp
+++ Gyak01/Pelda-02.cpp
@@ -4,16 +4,23 @@
 #include <iostream>
 
 void unknown(int* p, int num) {
-	int* q = &num;
-	*p = *q + 2;
+	// p trouble 1. indexére mutat
+	// num = 9
+	int* q = &num; // q egy új pointer, ami num-ra mutat
+	*p = *q + 2; // trouble = [9, 11, 9]
 	num = 4;
 }
 
 void htf(int* p, int q, int* num) {
-	*p = q + *num;
-	*num = q;
-	num = p;
-	p = &q;
+	// p a trouble 1. indexére mutat
+	// q = 9
+	// num a trouble 2. indexére mutat
+	// trouble jelenleg [9, 3, 22]
+	*p = q + *num; // p által mutatott érték 31
+	// ergo trouble jelenleg [9, 31, 22]
+	*num = q; // trouble jelenleg [9, 31, 9]
+	num = p; // num mostantól trouble 1. indexére mutat
+	p = &q; // p mostantól az itteni q változóra mutat (htf-q)
 	unknown(num, *p);
 }
 
@@ -25,7 +32,7 @@ int main()
 	q = &trouble[1];
 	*q = 3;
 	trouble[2] = 22;
-	htf(q, trouble[0], &trouble[2]);
+	htf(q, trouble[0], &trouble[2]); // trouble = [9, 11, 9]
 	std::cout << *q << std::endl;
 	std::cout << trouble[0] << std::endl;
 	std::cout << trouble[1] << std::endl;
