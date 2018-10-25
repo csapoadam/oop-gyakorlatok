651dbeb32515613072dbada07296ef1ef57ca280
Adam Csapo <csapo.adam@gmail.com>
2018-10-25 09:21:25 +0200

    back to Gyak05 final state to demonstrate virtual inheritance

---
+++ Gyak08/EastWorldEntities.h
@@ -0,0 +1,88 @@
+#ifndef EAST_WORLD_ENTITIES_HPP
+#define EAST_WORLD_ENTITIES_HPP
+
+#include <iostream>
+#include <string>
+#include <ctime>
+
+class EastWorldEntity {
+public:
+	EastWorldEntity(const std::string n) : name(n) {}
+	void whatsYourName() {
+		std::cout << "My name is " << name << "... hello!" << std::endl;
+	}
+private:
+	const std::string name;
+	//szemelyigazolvany szam nincs, mert ilyen a robotoknak altalaban nincs
+	//(kiveve fake humaneknek)
+};
+
+class Robot : public EastWorldEntity { // public orokles, mert Robot egyfajta Entity!!
+									   // (interfeszt oroklunk, nem implementaciot hasznalunk fel ujra / rejtunk el)
+public:
+	/*
+	Robot(const std::string name) {
+		this->name = name; //nem jo, mert konstanst valtozot inicializalni kell!!
+		//Raadasul name a szuloben private is szoval... innen nem elerheto
+	};
+	*/
+	// minden robot objektum tartalmaz egy Entity-t is a mem.teruleten
+	//tehat az entity-t is letre kell hozni
+	Robot(const std::string name) : EastWorldEntity(name) {
+		std::srand(std::clock()); //ha kikommentelem, mindig ugyanannyi lesz az elso, masodik, stb. charge. Ki lehet probalni!
+		batteryCharge = std::rand() % maximumCharge;
+	}
+	void recharge(int charge) {
+		if (charge > maximumCharge) {
+			batteryCharge = maximumCharge;
+		}
+		else {
+			if (charge > 0) batteryCharge = charge;
+		}
+	};
+protected: // ez es szarmazo osztalyok elerik, de kivulrol nem lehet
+	// ezzel szemben: public: szarmazo osztalyokbol ES kivulrol is elerheto
+	// ezzel szemben: private: sem szarmazo osztalyokbol, sem kivulrol NEM elerheto
+	// Stroustrup: protectedet csak fuggvenyekre hasznaljunk. Valtozoknal mindig kerdes, hogy miert protected??
+	// ha protected -> ki fogja irni? Ha senki, miert protected?
+	int getCharge() {
+		return batteryCharge;
+	}
+private:
+	static const int maximumCharge = 100;
+	int batteryCharge; // if zero, needs to recharge!
+};
+
+
+class ServantBot : public Robot {
+public:
+	ServantBot(std::string name) : Robot(name) {}
+	void whatsYourName() { //a szuloben is van ilyen fv, attol fugg, melyiket hivjuk meg, h melyik fut le
+		// name sajnos private
+		// std::cout << "Hi Master, my name is " << name << "... what can I do for you?" << std::endl;
+		std::cout << "Hi, I'm a servant bot, but I forgot my name. However, I can tell you my battery level is: " << getCharge() << std::endl;
+	}
+};
+
+class BehavesLikeHuman { // nem szarmazik EastWorldEntitybol, ld. lentebb, h miert
+public:
+	BehavesLikeHuman(std::string nid) : nationalid(nid) {}
+private:
+	const std::string nationalid;
+};
+
+class FakeHuman : public Robot, BehavesLikeHuman {
+	// megj: BehavesLikeHuman azert nem szarmazik EastWorldEntitybol, mert
+	// akkor Roboton es BehavesLikeHumanen keresztul FakeHuman ket kulonbozo
+	// EastWorldEntity-t is tartalmazna (hires diamond shape problem)
+	// Ez tulmutat a targy keretein, de veszelyes mert akkor 2 name lenne pl.
+public:
+	FakeHuman(std::string name, std::string nid) : Robot(name), BehavesLikeHuman(nid) {}
+};
+
+class Human : public EastWorldEntity, BehavesLikeHuman {
+public:
+	Human(std::string name, std::string nid) : EastWorldEntity(name), BehavesLikeHuman(nid) {}
+};
+
+#endif // !EAST_WORLD_ENTITIES_HPP
---
+++ Gyak08/main.cpp
@@ -0,0 +1,43 @@
+// EastWorldProject.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "EastWorldEntities.h"
+#include <iostream>
+#include <vector>
+
+int main() {
+	EastWorldEntity ewe1("Who Am I");
+	// std::cout << ewe1.name << std::endl; name private, vagyis nem hozzaferheto kivulrol
+	Robot rob1("Robi");
+	ServantBot sb1("Hello Bello");
+	// sb1.getCharge(); protected, ezert inaccessible kivulrol
+	FakeHuman fh("Tisztara Ember", "062255AB");
+	Human hum("Igazi Ember", "252525PK");
+
+	ewe1.whatsYourName();
+	rob1.whatsYourName();
+	sb1.whatsYourName();
+	fh.whatsYourName();
+	hum.whatsYourName();
+
+	std::vector<EastWorldEntity*> EastWorldEntityCollection;
+	EastWorldEntityCollection.push_back(&ewe1);
+	EastWorldEntityCollection.push_back(&rob1);
+	EastWorldEntityCollection.push_back(&sb1);
+	EastWorldEntityCollection.push_back(&fh);
+	EastWorldEntityCollection.push_back(&hum);
+
+	std::cout << "introduce entities through vector" << std::endl;
+	for (std::vector<EastWorldEntity*>::iterator i = EastWorldEntityCollection.begin();
+		i < EastWorldEntityCollection.end(); i++) {
+		// sajnos sb1-re is EastWorldEntity whatsYourName() metodusa hivodik meg
+		// a futtatasi kornyezet ugyanis nem tudja, hogy ezek valojaban nem biztos, hogy tenylegesen EastWorldEntity-k
+		// hanem mondjuk ServantBot-ok vagy FakeHuman-ok is lehetnek.
+		(*i)->whatsYourName(); //kovetkezo oran maskepp megoldjuk!
+	}
+
+	char c;
+	std::cin >> c;
+    return 0;
+}
